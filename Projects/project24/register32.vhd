2d3a24a2a7df6c0e8a7165374882408e4306265b
Lasthunter657 <ali.redha.ali@gmail.com>
2022-05-15 20:58:00 +0300

    ERROR11

--- Projects/project24/register32.vhd
+++ Projects/project24/register32.vhd
@@ -1,13 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
-ENTITY register32 IS 
+ENTITY REG IS 
 	PORT (
 		clk, en: IN std_logic;
 		datain: IN std_logic_vector(31 DOWNTO 0);
 		dataout: OUT std_logic_vector(31 DOWNTO 0)
 	);
 END ENTITY;
-ARCHITECTURE reg OF register32 IS
+ARCHITECTURE reg OF REG IS
 BEGIN
 	PROCESS (clk) IS
 	BEGIN
