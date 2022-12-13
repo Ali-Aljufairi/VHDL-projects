60e058c5b41362d6d2f06c579b3b37a6fb495f15
Ali Redha <85583841+Ali-Aljufairi@users.noreply.github.com>
2022-12-13 23:48:25 +0300

    delete this

--- Projects/Project33/Arom_inst.vhd
+++
@@ -1,7 +0,0 @@
-Arom_inst : Arom PORT MAP (
-		address_a	 => address_a_sig,
-		address_b	 => address_b_sig,
-		clock	 => clock_sig,
-		q_a	 => q_a_sig,
-		q_b	 => q_b_sig
-	);
--- Projects/Project33/Brom_inst.vhd
+++
@@ -1,7 +0,0 @@
-Brom_inst : Brom PORT MAP (
-		address_a	 => address_a_sig,
-		address_b	 => address_b_sig,
-		clock	 => clock_sig,
-		q_a	 => q_a_sig,
-		q_b	 => q_b_sig
-	);
